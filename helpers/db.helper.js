70c48ed56f938708eccded9a12550cae887381ff
Saurabh <saurabh96.in@gmail.com>
2021-04-05 18:33:52 +0530

    helper update

--- helpers/db.helper.js
+++ helpers/db.helper.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const dbConfig = require('../configs/db.config');
 
-// const dbUrl = "mongo "mongodb://cluster0-shard-00-00.wfbvm.mongodb.net:27017,cluster0-shard-00-01.wfbvm.mongodb.net:27017,cluster0-shard-00-02.wfbvm.mongodb.net:27017/twitter_bot?replicaSet=atlas-cdzsan-shard-0" --ssl --authenticationDatabase admin --username saurabh --password saurabh123";
-//mongo "mongodb://cluster0-shard-00-00.wfbvm.mongodb.net:27017,cluster0-shard-00-01.wfbvm.mongodb.net:27017,cluster0-shard-00-02.wfbvm.mongodb.net:27017/myFirstDatabase?replicaSet=atlas-cdzsan-shard-0" --ssl --authenticationDatabase admin --username saurabh --password <password>
-
 mongoose.connect(process.env.MONGODB_URI || dbConfig.connection_string, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
     err => {
         if (!err) {
