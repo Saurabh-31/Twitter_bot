78268bff02dc51db052b9b3405d91fb23f3b8f6e
Saurabh <saurabh96.in@gmail.com>
2021-04-07 22:16:28 +0530

    deploy5

--- routes/tweets.route.js
+++ routes/tweets.route.js
@@ -5,6 +5,11 @@
 const express = require('express'),
     tweetRouter = express.Router();
 
+    /* GET home page. */
+tweetRouter.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 // tweetRouter.route('/followSomeone').post(tweetController.followSomeone);
 // tweetRouter.route('/getFollowersList').post(tweetController.getFollowersList);
 // tweetRouter.route('/getFollowingList').post(tweetController.getFollowingList);
